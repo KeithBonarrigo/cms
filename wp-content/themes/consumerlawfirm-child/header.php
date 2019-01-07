f6d02a1153e842c84849b128695f62902b520b3a
Keith Bonarrigo <keith@keithbonline.com>
2019-01-07 12:56:24 -0800

    consolidating css from header social icons development

--- wp-content/themes/consumerlawfirm-child/header.php
+++ wp-content/themes/consumerlawfirm-child/header.php
@@ -31,97 +31,8 @@
         <!-- End of LiveChat code -->
          
         <?php wp_head(); ?>
-        <style>
-        .facebook_left, .twitter_left, .linkedin_left, .gplus_left, .email_left { margin-bottom:2px; position:relative; top:0px; left:0px; height:35px !important; width:30px !important;  }
-        .facebook_left #facebook_icon, .twitter_left #twitter_icon, .linkedin_left #linkedin_icon, .gplus_left #gplus_icon, .email_left #email_icon { background-size: 30px 35px; height:35px; width:30px; right:0px !important  }
-        .linkedin_left #linkedin_icon { top:0px; background-image: url('https://consumerlawfirmcenter.com/cms/wp-content/uploads/2018/11/linked_in_logo.png'); background-repeat:no-repeat;  }
-        .email_left #email_icon { top: 0px; background-image: url('https://consumerlawfirmcenter.com/cms/wp-content/uploads/2018/11/email-icon-trans.png'); background-repeat:no-repeat; /*background-size:44px 50px;*/  }
-        .side-social { border-radius:0px 7px 7px 0px; }
-        .has-social { display:none !important}
-        .social-container { width: 35px; height: 389px; position: fixed; left: -35px; top:260px; z-index: 99997; }
-
-        .contact-area .contact-block { font-family:'EB Garamond'; font-weight:bold; letter-spacing: 1px; }
-        .contact-info-holder { width:420px !important }
-        .contact-block-phone { text-decoration:none; color: #7496AB; }
-        .has-social { display:none !important }
-        .site-info h3 { font-weight: bold !important; }
-        .site-info, .site-info h3 { font-family:'EB Garamond' !important; }
-
-        #homepage-header { text-align:center; margin:17px auto; color: #333333; font-weight: bold; }
-        .homepage-icon-holder { padding:14px 10px 10px 10px; text-align: center; background-color:#ece8e8; }
-        .tile-holder { text-align: center; background-color:#ece8e8; border:solid 1px #ccc; padding-left:0px !important; padding-right:0px !important }
-        .tile-header { background-color:#ED5A5A; color:#fff; font-weight: bold; padding: 10px 7px 7px 7px; }
-        .tile-content { text-align: left; margin-top:15px; text-indent: 7px; line-height:1.3; }
-        .tile-button a { margin-bottom: 0px !important; }
-        .home-content-lower { padding: 7px 10px; font-size: 16px; text-indent: 7px; }
-        .lower-content-learn-more { text-decoration: none; color: #ED5A5A }
-
-        .contact-block { display:block !important; }
-        .contact-info-holder { padding-left:40px; float:right; margin-right:-95px; margin-top:10px; width:400px; text-align: left; }
-        .contact-info-holder .contact-block { text-align: left; line-height:1.5; font-size: 15px; }
-        .admin-bar .site-header.header-scrolled { top:0px !important; }
-        .contact-area .contact-block:first-child a { font-size:15px; font-weight: bold; }
-        .content-indented { margin-left:8px !important; }
-        #free-case-review-upper { text-decoration:none; font-weight:bold; background-color: #ed5a5a !important; color:#fff; padding:4px 5px !important; margin:0px; text-transform: uppercase;  }
-    
-        .call-now, .cta { margin-left:20px !important; }
-        .call-now { margin-top:40px !important }
-        .cta { background-color:#ed5a5a; color:#FFF !important; }
-        .banner .call-now .number { margin-top:15px !important; }
-        .header-img__content { margin-left: 15px; margin-top: -30px; }
-        .banner .cta { margin-top:0px !important; }
-        .header-img { border:solid 1px #d7d0d0; border-width: 1px 0px; }
-        
-
-        .home-video-holder { margin-top:30px; }
-        .home-video-header { margin-left:auto; margin-right:auto; font-family: 'EB Garamond'; width:500px; border:solid 1px #ED5A5A; border-width:0px 0px 2px 0px; font-size:16px; letter-spacing: 1px; background-color:#333333; color: #eae5e5; padding:5px 5px 5px 7px; text-align: center; font-weight: 900;  }
-        .home-video-holder .videobox { margin-top:10px; }
-        .home-video-holder .videobox .wp-video, .home-video-holder .videobox .wp-video-shortcode { margin:-18px auto 0px auto !important; }
-
-        hr.lower-breaker { border: none;  height: 1px; /* Set the hr color */ background-color: #9496A2; /* old IE */ }
-        .testimonials { padding:0px 65px 0px 45px !important; margin-top:105px; }
-        blockquote.easy_testimonial { text-align: left; }
-        .easy_t_single_testimonial .easy_testimonial_title { font-weight:bold !important; }
-        
-        .home-page-contact input[type='text'],.home-page-contact input[type='email'], .home-page-contact select, .home-page-contact textarea  { padding:3px !important; border:1px solid #CCC; background-color:#F7F7F7 }
-        .home-page-contact textarea  { height:100px; }
-        .home-page-contact input[type='submit'] { padding:10px 35px; }
-        blockquote.easy_testimonial:first-child { margin-top:-20px; }
-        blockquote.easy_testimonial p { font-size:15px; font-style:italic; }
-        .easy_t_single_testimonial .easy_testimonial_title { font-size:15px; margin:-12px 0px 0px 28px !important; font-size: 15px; }
-        .easy_t_single_testimonial .easy_testimonial_title span.date { font-weight:normal; font-size: 14px !important }
-        .easy_testimonial_title { padding-left:35px; }
-        .testimonial_body p { padding-bottom: 5px !important; }
-
-        blockquote::before { content:none !important; }
-        blockquote.easy_testimonial .testimonial_body { margin-top:40px; }
-        blockquote.easy_testimonial .testimonial-body .fa-quote-left { color:#ED5A5A !important; }
-        blockquote.easy_testimonial .testimonial-body .fa-quote-left:before { color:#ED5A5A !important; font-family: 'Font Awesome\ 5 Free' !important; font-weight: bold !important; content: 'f10d' !important; }
-        blockquote.easy_testimonial .testimonial-body .fa-quote-right:after { color:#ED5A5A !important; font-family: 'Font Awesome\ 5 Free' !important; font-weight: bold !important; content: 'f10e' !important; }
-        .cfc-quote { color:#ED5A5A !important; }
-        .testimonial_author { padding:5px 0px 0px 25px;  }
-        .testimonial_author .date { display:none; }
-        #lower-homepage-bbb { margin:95px 0px 60px 0px; }
-        #lower-homepage-bbb ._txt-center { margin-top:8px; }
-    </style>
     </head>
     <body <?php body_class(); ?>>
-    
-    <!--<div class="facebook_left">
-        <div id="facebook_icon" style="height:48px !important; width:40px !important"></div>
-    </div>
-    <div class="twitter_left">
-        <div id="twitter_icon" style="height:48px !important; width:40px !important"></div>
-    </div>
-    <div class="gplus_left">
-        <div id="gplus_icon"></div>
-    </div>
-    <div class="linkedin_left">
-            <div id="linkedin_icon" class="side-social"></div>
-	</div>
-	<div class="email_left">
-		<div id="email_icon" class="side-social"></div>
-	</div>-->
     <div class='social-container'>
 		<div class="facebook_left">
 			<div id="facebook_icon" class="side-social"></div>
@@ -143,14 +54,7 @@
 
     <script>
         jQuery(document).ready(function($) {
-            /*$(".facebook_left").animate({left: "-265" }, 1300 );
-            $(".twitter_left").animate({left: "-265" }, 1300 );
-            $(".gplus_left").animate({left: "-265" }, 1300 );
-            $(".linkedin_left").animate({left: "-265" }, 1300 );
-            $(".email_left").animate({left: "-265" }, 1300 );*/
             $(".social-container").animate({left: "0" }, 1300 );
-
-
             $(".facebook_left").click(function(){
                 window.open("https://www.facebook.com/ConsumerRightsLawFirm", 'fb');
             });
@@ -179,8 +83,6 @@
             var new_height = max_height + "px";
             $(".tile-content").css('min-height', new_height)
             /* end tile heights*/
-
-
         })
     /*--end social media sliding buttons--*/
     </script>
@@ -203,20 +105,6 @@
                     <img src="<?php bloginfo('template_url') ?>/images/logo.png" alt="Consumer Law Firm" width="300" height="60" border="0">
                 </a>
 				 <?php // echo do_shortcode('[prisna-google-website-translator]'); ?>
-
-                <!--<div class="contact-area" style="float: right;">
-                    <div class="contact-info">
-                        <div class="contact-block">
-                            <i class="tora-icon dslc-icon-ei-icon_phone">&nbsp;</i>(877) 700-5790
-                        </div>
-                        <div class="contact-block">
-                            <a href="mailto:help@consumerlawfirmcenter.com">help@consumerlawfirmcenter.com</a>
-                        </div>
-                        <div class="contact-block">
-                            <i class="tora-icon dslc-icon-ei-icon_pin_alt">&nbsp;</i>United States 
-                        </div>
-                    </div>
-                </div>-->
                 <div class="contact-area" style="float: right;">
                     <div class="contact-info">
                         <div class='contact-info-holder'>
@@ -228,10 +116,7 @@
                             </div>
                             <div class="contact-block content-indented">
                                 <i class="fas fa-envelope-square" style="color:black">&nbsp;</i>&nbsp; <a id="header-email-link" style="color:#ed5a5a" href="mailto:help@consumerlawfirmcenter.com">help@consumerlawfirmcenter.com</a>
-                            </div>
-                            <!--<div class="contact-block content-indented">
-                                <i class="fas fa-map-marked" style="color:black">&nbsp;</i>&nbsp;<a style="color:#ed5a5a" href="https://www.google.com/maps/place/Consumer+Rights+Law+Firm+PLLC/@42.702374,-71.1328997,17z/data=!3m1!4b1!4m5!3m4!1s0x89e30650e98fba1f:0x33aeb4dd383c2358!8m2!3d42.702374!4d-71.130711">North Andover, Mass. USA</a> 
-                            </div>-->    
+                            </div>    
                         </div>
                     </div>
                 </div>
@@ -241,29 +126,9 @@
             </div>
 
         </div>
-        <?php // do_action('tora_header_image');?>
         <!--begin tora header--> 
         <?php do_action('tora_header_bar'); ?>
-        
-        <?php // if ( is_page( 'Home Page' )) echo do_shortcode('[layerslider id="1"]'); ?>
-
         <?php if ( is_page( 'Home Page' )) { ?>
-            <!--<div class="header-img banner" >
-                <img src="<?php bloginfo('template_url') ?>/images/banner.jpg" style="width:100%" alt="Debt Collection Programs">
-                <div class="container">
-                    <div class="header-img__content">
-                        <h1>Debt collector Harassment is against the law!</h1>
-                        <h2>We helped <span class="stand-out-text">1,000’s</span> of client at zero cost.  Let’s stop the calls today </h2>
-                        <div class="call-now">
-                            <span class="label">Call now at:</span>
-                            <span class="number">(877) 700-5790</span>
-                        </div>
-                        <a class="cta" href="#freeCaseReview">
-                            Free Case Review
-                        </a>
-                    </div>
-                </div>
-            </div>-->
             <div class="header-img banner" >
                 <img src="https://consumerlawfirmcenter.com/cms/wp-content/themes/consumerlawfirm/images/banner.jpg" style="width:100%" alt="Debt Collection Programs">
                 <div class="container" style="text-align: center">
